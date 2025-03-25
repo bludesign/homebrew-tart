2d998b7bdf88c279f6905b9f4bc38f5b6319d547
Chandler Huff <contact@bludesign.biz>
2025-03-24 21:52:34 -0400

    Add Tart Executor

--- tart-executor.rb
+++ tart-executor.rb
@@ -1,18 +1,18 @@
 class TartExecutor < Formula
-    desc "Tart executor for github"
-    homepage "https://github.com/chandlerhuff/tartelet"
-    url "https://github.com/chandlerhuff/tartelet/releases/download/v0.12.11/tart-executor.zip"
-    sha256 "9d2c63714396a853a7f15e17a0d6ca53792714d7efb72c3c189bb7d803272ed5"
-    license "MIT"
+  desc "Tart executor for github"
+  homepage "https://github.com/bludesign/tartelet"
+  url "https://github.com/bludesign/tartelet/releases/download/v0.12.16/tart-executor.zip"
+  sha256 "9d2c63714396a853a7f15e17a0d6ca53792714d7efb72c3c189bb7d803272ed5"
+  license "MIT"
 
-    def install
-      bin.install "tart-executor"
-    end
-  
-    service do
-      run [opt_bin/"tart-executor"]
-      keep_alive false
-      log_path var/"log/tart-executor.log"
-      error_log_path var/"log/tart-executor.err.log"
-    end
+  def install
+    bin.install "tart-executor"
   end
+
+  service do
+    run [opt_bin/"tart-executor"]
+    keep_alive false
+    log_path var/"log/tart-executor.log"
+    error_log_path var/"log/tart-executor.err.log"
+  end
+end
