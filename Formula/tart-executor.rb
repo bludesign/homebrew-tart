a9ef8383f61f4d356a416e3f81dabfd8a49576af
Chandler Huff <contact@bludesign.biz>
2025-04-05 13:58:26 -0400

    Add Tart Executor

---
+++ Formula/tart-executor.rb
@@ -0,0 +1,18 @@
+class TartExecutor < Formula
+  desc "Tart executor for github"
+  homepage "https://github.com/bludesign/tart-executor"
+  url "https://github.com/bludesign/tart-executor/releases/download/v0.12.28/tart-executor.zip"
+  sha256 "9bc309a32ffcb3d34010db77b9b1e1fe4104995fe0bcd523613751f01b55147d"
+  license "MIT"
+
+  def install
+    bin.install "tart-executor"
+  end
+
+  service do
+    run [opt_bin/"tart-executor"]
+    keep_alive false
+    log_path var/"log/tart-executor.log"
+    error_log_path var/"log/tart-executor.err.log"
+  end
+end
---
+++ Formula/tart-router.rb
@@ -0,0 +1,18 @@
+class TartRouter < Formula
+  desc "Tart router for github"
+  homepage "https://github.com/bludesign/tart-executor"
+  url "https://github.com/bludesign/tart-executor/releases/download/v0.12.28/tart-router.zip"
+  sha256 "6da25d1608259ed171ee90e89a5aad792104d69baff32222521c963e46ee123b"
+  license "MIT"
+
+  def install
+    bin.install "tart-router"
+  end
+
+  service do
+    run [opt_bin/"tart-router"]
+    keep_alive false
+    log_path var/"log/tart-router.log"
+    error_log_path var/"log/tart-router.err.log"
+  end
+end
