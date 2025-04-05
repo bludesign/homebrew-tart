a90b1316b944369213769a17eb0c2bdff120cb5b
Chandler Huff <contact@bludesign.biz>
2025-04-05 14:20:54 -0400

    tart-executor 0.12.30
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/tart-executor.rb
+++ Formula/tart-executor.rb
@@ -1,8 +1,8 @@
 class TartExecutor < Formula
   desc "Tart executor for github"
   homepage "https://github.com/bludesign/tart-executor"
-  url "https://github.com/bludesign/tart-executor/releases/download/v0.12.28/tart-executor.zip"
-  sha256 "9bc309a32ffcb3d34010db77b9b1e1fe4104995fe0bcd523613751f01b55147d"
+  url "https://github.com/bludesign/tart-executor/releases/download/v0.12.30/tart-executor.zip"
+  sha256 "9435c1e92d462b138a24d87ec65c3e3894399167632e02d72701f81dc9ff2649"
   license "MIT"
 
   def install
