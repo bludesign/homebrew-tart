1d7b07383823da8aebdc0e8edd7aade0ada4b3fc
Chandler Huff <contact@bludesign.biz>
2025-04-17 20:47:48 -0400

    tart-router 0.12.32
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/tart-router.rb
+++ Formula/tart-router.rb
@@ -1,8 +1,8 @@
 class TartRouter < Formula
   desc "Tart router for github"
   homepage "https://github.com/bludesign/tart-executor"
-  url "https://github.com/bludesign/tart-executor/releases/download/v0.12.31/tart-router.zip"
-  sha256 "031bea90b2677c5bd68a9441832a2da313af5f6625c52bd1dcfc6ec249b6f1a8"
+  url "https://github.com/bludesign/tart-executor/releases/download/v0.12.32/tart-router.zip"
+  sha256 "f1d6f04027ef612056da067056aa3c79a47a6da488d1ae6a0290e9ab565d4b5e"
   license "MIT"
 
   def install
